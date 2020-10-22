e07ad488a8b13e0bf2b1f6a887a9cd42af95991a
bhodgs <bhodgs@deakin.edu.au>
2020-10-22 23:19:08 +1100

    split the init vars into a separate file

--- src/core/vendor/three.js
+++ src/core/vendor/three.js
@@ -1,140 +1,66 @@
-import * as $ from "jquery";
-import * as THREE from "three";
-import './proctree.js';
-import { ClampToEdgeWrapping, WebGLRenderer } from "three";
+import * as $ from 'jquery'
+import * as THREE from 'three'
+import './proctree.js'
+import { three } from './three-init.js'
+import { ClampToEdgeWrapping, WebGLRenderer } from 'three'
 
+export class ThreeRuntime {
+  camera;
+  renderer;
+  scene;
+  light;
+  trunkGeo;
+  trunkMesh;
+  tree;
 
-var scene = new THREE.Scene();
-var camera = new THREE.PerspectiveCamera(
- 75,
- window.innerWidth / window.innerHeight,
- 0.1,
- 1000
-);
-var renderer = new WebGLRenderer();
-var geometry = new THREE.BoxGeometry();
-var material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
-var light = new THREE.AmbientLight( 0x404040 ); // soft white light
+  constructor (threeJSON) {
+   Object.assign(this, threeJSON);
+  }
 
-var cube = new THREE.Mesh( geometry, material );
-var cubes = [];
-var tree = new window.Tree(
-    {
-		"seed": 262,
-		"segments": 6,
-		"levels": 5,
-		"vMultiplier": 2.36,
-		"twigScale": 0.39,
-		"initalBranchLength": 0.49,
-		"lengthFalloffFactor": 0.85,
-		"lengthFalloffPower": 0.99,
-		"clumpMax": 0.454,
-		"clumpMin": 0.404,
-		"branchFactor": 2.45,
-		"dropAmount": -0.1,
-		"growAmount": 0.235,
-		"sweepAmount": 0.01,
-		"maxRadius": 0.139,
-		"climbRate": 0.371,
-		"trunkKink": 0.093,
-		"treeSteps": 5,
-		"taperRate": 0.947,
-		"radiusFalloffRate": 0.73,
-		"twistRate": 3.02,
-		"trunkLength": 2.4
-	}
+  InitThree () {
+    this.camera.position.set(0, 5, 5)
+    this.trunkGeo.castShadow = true
+    this.trunkMesh.position.setY(-3)
+    this.light.position.set(4, -1, 0)
+    this.light.castShadow = true
 
-)
+    this.renderer.setSize(window.innerWidth, window.innerHeight)
+    this.renderer.domElement.setAttribute('class', '')
+    this.renderer.shadowMap.enabled = true
+    this.renderer.shadowMap.type = THREE.PCFSoftShadowMap
 
-    var trunkGeo = newTreeGeometry(tree);
-    var trunkMaterial = new THREE.MeshLambertMaterial( { color: 0x00ff00, wireframe: false } );
-    var trunkMesh = new THREE.Mesh(trunkGeo, trunkMaterial);
+    $('.Content').append(this.renderer.domElement)
 
+    this.scene.background = new THREE.Color('skyblue')
+    this.scene.add(this.light)
 
+    this.renderTree(this.tree)
+    this.moveIn()
+  }
 
-
-
-
-export function InitThree() {
-
-  camera.position.set(0, 5, 5);
-  trunkGeo.castShadow = true;
-  trunkMesh.position.setY(-3);
-  light.position.set( 4, -1, 0 );
-  light.castShadow = true; 
-
-  renderer.setSize(window.innerWidth, window.innerHeight);
-  renderer.domElement.setAttribute("class", "three");
-  renderer.shadowMap.enabled = true;
-  renderer.shadowMap.type = THREE.PCFSoftShadowMap; 
-
-   $('.Content').append(renderer.domElement);
-  
-  scene.background = new THREE.Color('skyblue');
-  scene.add( light );
-
-  renderTree(tree);
-  moveIn();
-}
-
-function renderTree(){
-    scene.add(trunkMesh); // Use your own scene
-}
-
-function newTreeGeometry(tree, isTwigs) {
-    var output = new THREE.Geometry();
-  
-    tree[ isTwigs ? 'vertsTwig' : 'verts'].forEach(function(v) {
-      output.vertices.push(new THREE.Vector3(v[0], v[1], v[2]));
-    });
-  
-    var uv = isTwigs ? tree.uvsTwig : tree.UV;
-    tree[ isTwigs ? 'facesTwig' : 'faces'].forEach(function(f) {
-      output.faces.push(new THREE.Face3(f[0], f[1], f[2]));
-      output.faceVertexUvs[0].push(f.map(function(v) {
-        return new THREE.Vector2(uv[v][0], uv[v][1]);
-      }));
-    });
-  
-    output.computeFaceNormals();
-    output.computeVertexNormals(true);
-  
-    return output;
+  renderTree () {
+    this.scene.add(this.trunkMesh) // Use your own scene
   }
 
 
-function generateCube(){
-   return new THREE.Mesh( geometry, material );
-}
+  generateCube () {
+    return new THREE.Mesh(this.geometry, this.material)
+  }
 
 
-function spawn(){
+  render () {
+    requestAnimationFrame(this.render)
+    this.trunkGeo.rotateY(0.01)
+    this.renderer.render(this.scene, this.camera)
+  }
 
-    for(var i = 0; i < 100; i++){
-        var cube = generateCube();
-        cube.position.x = i - 50;
-        cubes.push(cube);
+  moveIn () {
+    var handle = requestAnimationFrame(this.moveIn)
+    this.camera.translateY(-0.01)
+    this.renderer.render(this.scene, this.camera)
+    if (this.camera.position.y < 1) {
+      cancelAnimationFrame(handle)
+      this.render()
     }
-    cubes.forEach(function(cube){
-        scene.add(cube);
-    })
-    
-
-}
-
-function render(){
-    requestAnimationFrame( render );
-    trunkGeo.rotateY(0.01);
-    renderer.render( scene, camera );
-}
-
-
-function moveIn(){ 
-    var handle = requestAnimationFrame( moveIn );
-    camera.translateY(-0.01);
-    renderer.render( scene, camera );
-    if(camera.position.y < 1){
-       cancelAnimationFrame( handle )
-       render();
-    }
+  }
 }
